aeb17d5a284a72a70517b7f6aac3cc5473c00782
Ezequiel Martin Apfel <ezequiel.apfel@gmail.com>
2011-10-11 15:15:07 -0300

    .

--- promote-production.sh
+++ promote-production.sh
@@ -1,23 +1,24 @@
 #!/bin/sh
 
-echo "Promoviendo a producction desde el tag jenkins-agile2011-last-${PROMOTED_NUMBER}"
+echo "Promoviendo a producction desde el tag jenkins-agiles2011-last-${PROMOTED_NUMBER}"
 
 cd ..
 
-if [ ! -d jenkins-agile2011-last-${PROMOTED_NUMBER} ];
+if [ ! -d jenkins-agiles2011-last-${PROMOTED_NUMBER} ];
 then
-git clone git@github.com:eapfel/agiles2011.git jenkins-agile2011-last-${PROMOTED_NUMBER}
+git clone git@github.com:eapfel/agiles2011.git jenkins-agiles2011-last-${PROMOTED_NUMBER}
 fi
 
-cd jenkins-agile2011-last-${PROMOTED_NUMBER}
+cd jenkins-agiles2011-last-${PROMOTED_NUMBER}
 
 if [ -z `git remote | grep heroku-prod` ];
 then
+echo "Agregando heroku remote"
 git remote add heroku-prod git@heroku.com:agiles2011.git
 fi
 
 heroku maintenance:on --app agiles2011
 
-git push heroku-prod master
+git push heroku-prod master:master
 
 heroku maintenance:off --app agiles2011
